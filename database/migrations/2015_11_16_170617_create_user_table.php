f0e28666d23d2d2aa1e69580e1970d761c97a67e
Stephen <stephen.frx@gmail.com>
2015-11-16 17:29:37 +0800

    Add user table

---
+++ database/migrations/2015_11_16_170617_create_user_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up() {
+		Schema::create('px_user', function (Blueprint $table) {
+			$table->increments('id');
+			$table->string('username', 60)->unique();
+			$table->string('email', 255)->unique()->nullable();
+			$table->string('password', 255);
+			$table->string('realname')->nullable();
+			$table->integer('department_id');
+			$table->boolean('activated')->default(0);
+			$table->rememberToken();
+			$table->timestamp('login_at')->nullable();
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down() {
+		Schema::drop('px_user');
+	}
+
+}
