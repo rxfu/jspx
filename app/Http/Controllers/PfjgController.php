75ca383372ae61d6915a344bd076b25ab3e4bf63
Stephen <stephen.frx@gmail.com>
2015-12-04 13:47:57 +0800

    Modify pfjg controller function multiplex

--- app/Http/Controllers/PfjgController.php
+++ app/Http/Controllers/PfjgController.php
@@ -8,12 +8,8 @@ use Maatwebsite\Excel\Facades\Excel;
 
 class PfjgController extends AdminController {
 
-	public function getMonitor() {
-		$year  = System::find('CJ_WEB_ND')->value;
-		$term  = System::find('CJ_WEB_XQ')->value;
-		$title = $year . '~' . ($year + 1) . '年度' . Term::find($term)->mc . '学期评价监控';
-
-		$results = DB::table('pk_jxrw')
+	protected function retrieveMonitorResults($year, $term) {
+		return DB::table('pk_jxrw')
 			->join('pk_js', 'pk_jxrw.jsgh', '=', 'pk_js.jsgh')
 			->join('xt_department', 'pk_js.xy', '=', 'xt_department.dw')
 			->leftJoin('px_pfjg', function ($join) {
@@ -30,31 +26,10 @@ class PfjgController extends AdminController {
 			->orderBy('pk_js.jsgh', 'asc')
 			->orderBy('count', 'asc')
 			->get();
-
-		return view('pfjg.monitor', ['title' => $title, 'results' => $results]);
 	}
 
-	public function getStatistics($year, $term) {
-		if (!Auth::user()->groups[0]->permissions->contains('pfjg.statistics')) {
-			if (!Auth::user()->groups[0]->permissions->contains('pfjg.departmentStatistics')) {
-				if (!Auth::user()->groups[0]->permissions->contains('pfjg.majorStatistics')) {
-					return redirect()->guest('auth/login');
-				} else {
-					$college = Auth::user()->department->mc;
-					$major   = Auth::user()->major->mc;
-					$grade   = Auth::user()->grade;
-				}
-			} else {
-				$college = Auth::user()->department->mc;
-			}
-		}
-		$title = $year . '~' . ($year + 1) . '年度' . Term::find($term)->mc . '学期';
-		$title .= isset($college) ? $college : '';
-		$title .= isset($major) ? $major . '专业' : '';
-		$title .= isset($grade) ? $grade . '级' : '';
-		$title .= '教师评学统计表';
-
-		$results = DB::table('pk_jxrw')
+	protected function retrieveStatisticsResults($year, $term) {
+		return DB::table('pk_jxrw')
 			->join('jx_kc', 'pk_jxrw.kch', '=', 'jx_kc.kch')
 			->join('pk_kczy', function ($join) {
 				$join->on('pk_jxrw.nd', '=', 'pk_kczy.nd')
@@ -91,31 +66,45 @@ class PfjgController extends AdminController {
 			->groupBy('pk_jxrw.nd', 'pk_jxrw.xq', 'pk_jxrw.kcxh', 'jx_kc.kcmc', 'pk_kczy.nj', 'pk_kczy.zy', 'jx_zy.mc', 'pk_kczy.kkxy', 'xt_department.mc')
 			->orderBy('pk_jxrw.kcxh')
 			->get();
+	}
+
+	public function getMonitor() {
+		$year    = System::find('CJ_WEB_ND')->value;
+		$term    = System::find('CJ_WEB_XQ')->value;
+		$title   = $year . '~' . ($year + 1) . '年度' . Term::find($term)->mc . '学期评价监控';
+		$results = $this->retrieveMonitorResults($year, $term);
+
+		return view('pfjg.monitor', ['title' => $title, 'results' => $results]);
+	}
+
+	public function getStatistics($year, $term) {
+		if (!Auth::user()->groups[0]->permissions->contains('pfjg.statistics')) {
+			if (!Auth::user()->groups[0]->permissions->contains('pfjg.departmentStatistics')) {
+				if (!Auth::user()->groups[0]->permissions->contains('pfjg.majorStatistics')) {
+					return redirect()->guest('auth/login');
+				} else {
+					$college = Auth::user()->department->mc;
+					$major   = Auth::user()->major->mc;
+					$grade   = Auth::user()->grade;
+				}
+			} else {
+				$college = Auth::user()->department->mc;
+			}
+		}
+		$title = $year . '~' . ($year + 1) . '年度' . Term::find($term)->mc . '学期';
+		$title .= isset($college) ? $college : '';
+		$title .= isset($major) ? $major . '专业' : '';
+		$title .= isset($grade) ? $grade . '级' : '';
+		$title .= '教师评学统计表';
+		$results = $this->retrieveStatisticsResults($year, $term);
 
 		return view('pfjg.statistics', ['title' => $title, 'year' => $year, 'term' => $term, 'results' => $results]);
 	}
 
 	public function getExportMonitor() {
-		$year = System::find('CJ_WEB_ND')->value;
-		$term = System::find('CJ_WEB_XQ')->value;
-
-		$results = DB::table('pk_jxrw')
-			->join('pk_js', 'pk_jxrw.jsgh', '=', 'pk_js.jsgh')
-			->join('xt_department', 'pk_js.xy', '=', 'xt_department.dw')
-			->leftJoin('px_pfjg', function ($join) {
-				$join->on('px_pfjg.jsgh', '=', 'pk_jxrw.jsgh')
-					->on('px_pfjg.kcxh', '=', 'pk_jxrw.kcxh')
-					->on('px_pfjg.nd', '=', 'pk_jxrw.nd')
-					->on('px_pfjg.xq', '=', 'pk_jxrw.xq');
-			})
-			->where('pk_jxrw.nd', '=', $year)
-			->where('pk_jxrw.xq', '=', $term)
-			->select('pk_jxrw.nd', 'pk_jxrw.xq', 'pk_jxrw.kcxh', 'pk_js.jsgh', 'pk_js.xm as jsxm', 'pk_js.xy as xyh', 'xt_department.mc as xymc')
-			->addSelect(DB::raw('count(t_px_pfjg.pjbz_id) as count'))
-			->groupBy('pk_jxrw.nd', 'pk_jxrw.xq', 'pk_jxrw.kcxh', 'pk_js.jsgh', 'pk_js.xm', 'pk_js.xy', 'xt_department.mc')
-			->orderBy('pk_js.jsgh', 'asc')
-			->orderBy('count', 'asc')
-			->get();
+		$year      = System::find('CJ_WEB_ND')->value;
+		$term      = System::find('CJ_WEB_XQ')->value;
+		$results   = $this->retrieveMonitorResults($year, $term);
 		$sheetName = $year . '~' . ($year + 1) . '学年度' . Term::find($term)->mc . '学期教师评学监控表';
 
 		$datas[0] = ['教师工号', '教师姓名', '所在学院', '课程序号', '已评数量'];
@@ -142,37 +131,7 @@ class PfjgController extends AdminController {
 	}
 
 	public function getExportStatistics($year, $term) {
-		$results = DB::table('pk_jxrw')
-			->join('jx_kc', 'pk_jxrw.kch', '=', 'jx_kc.kch')
-			->join('pk_kczy', function ($join) {
-				$join->on('pk_jxrw.nd', '=', 'pk_kczy.nd')
-					->on('pk_jxrw.xq', '=', 'pk_kczy.xq')
-					->on('pk_jxrw.kcxh', '=', 'pk_kczy.kcxh');
-			})
-			->join('jx_zy', 'pk_kczy.zy', '=', 'jx_zy.zy')
-			->join('xt_department', 'pk_kczy.kkxy', '=', 'xt_department.dw')
-			->leftJoin('px_pfjg', function ($join) {
-				$join->on('px_pfjg.jsgh', '=', 'pk_jxrw.jsgh')
-					->on('px_pfjg.kcxh', '=', 'pk_jxrw.kcxh')
-					->on('px_pfjg.nd', '=', 'pk_jxrw.nd')
-					->on('px_pfjg.xq', '=', 'pk_jxrw.xq');
-			})
-			->where('pk_jxrw.nd', '=', $year)
-			->where('pk_jxrw.xq', '=', $term)
-			->where(function ($query) {
-				if (Auth::user()->groups[0]->permissions->contains('pfjg.exportDepartmentStatistics')) {
-					$query->where('pk_kczy.kkxy', '=', Auth::user()->department_id);
-				} elseif (Auth::user()->groups[0]->permissions->contains('pfjg.exportMajorStatistics')) {
-					$query->where('pk_kczy.kkxy', '=', Auth::user()->department_id)
-						->where('pk_kczy.zy', '=', Auth::user()->major_id)
-						->where('pk_kczy.nj', '=', Auth::user()->grade);
-				}
-			})
-			->select('pk_jxrw.nd', 'pk_jxrw.xq', 'pk_jxrw.kcxh', 'jx_kc.kcmc', 'pk_kczy.nj', 'pk_kczy.zy', 'jx_zy.mc as zymc', 'pk_kczy.kkxy', 'xt_department.mc as xymc')
-			->addSelect(DB::raw('sum(t_px_pfjg.fz) / count(t_px_pfjg.jsgh) * count(distinct(t_px_pfjg.pjbz_id)) as total'))
-			->groupBy('pk_jxrw.nd', 'pk_jxrw.xq', 'pk_jxrw.kcxh', 'jx_kc.kcmc', 'pk_kczy.nj', 'pk_kczy.zy', 'jx_zy.mc', 'pk_kczy.kkxy', 'xt_department.mc')
-			->orderBy('pk_jxrw.kcxh')
-			->get();
+		$results   = $this->retrieveStatisticsResults($year, $term);
 		$sheetName = $year . '~' . ($year + 1) . '学年度' . Term::find($term)->mc . '学期教师评学统计表';
 
 		$datas[0] = ['课程序号', '课程名称', '开课学院', '专业', '年级', '总分'];
